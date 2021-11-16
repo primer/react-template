fa560e766b5c1b29eec1227e4a6e9955d57ff3a1
Maxime De Greve <maximedegreve@github.com>
2021-11-16 15:23:31 +0000

    Add icons

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,4 +1,8 @@
 import { useTheme, SelectMenu, Button, Box } from "@primer/components";
+import {
+  SunIcon,
+  MoonIcon,
+} from "@primer/octicons-react";
 
 function ColorModeSwitcher() {
   const { setDayScheme, setNightScheme, colorScheme, theme } = useTheme();
@@ -13,39 +17,45 @@ function ColorModeSwitcher() {
     {
       name: "Light",
       value: "light",
+      icon: SunIcon,
     },
     {
       name: "Light colorblind",
       value: "light_protanopia",
+      icon: SunIcon,
     },
     {
       name: "Dark",
       value: "dark",
+      icon: MoonIcon,
     },
     {
       name: "Dark colorblind",
       value: "dark_protanopia",
+      icon: MoonIcon,
     },
     {
       name: "Dark high contrast",
       value: "dark_high_contrast",
+      icon: MoonIcon,
     },
     {
       name: "Dark Dimmed",
       value: "dark_dimmed",
+      icon: MoonIcon,
     }
   ];
 
-  const currentName = schemes.find(
+  const current = schemes.find(
     (scheme) => scheme.value === colorScheme
-  ).name;
+  );
 
   return (
     <Box position="absolute" top={0} right={0} p={3}>
       <Box position="relative" display="flex" justifyContent="flex-end">
         <SelectMenu>
-          <Button variant="small" as="summary">
-            {currentName}
+          <Button as="summary" variant="small">
+          <current.icon /><Box display="inline-block" ml={2}> {current.name}</Box>
           </Button>
           <SelectMenu.Modal align="right">
             <SelectMenu.List>
@@ -56,7 +66,7 @@ function ColorModeSwitcher() {
                   selected={scheme.value === colorScheme}
                   onClick={() => setScheme(scheme.value)}
                 >
-                  {scheme.name}
+                {scheme.name}
                 </SelectMenu.Item>
               ))}
             </SelectMenu.List>
