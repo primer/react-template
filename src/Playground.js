1eb32e5ff662e86fd8cc79a6d23f331190ee31f6
Maxime De Greve <maximedegreve@github.com>
2023-08-21 18:35:11 +0100

    Upgrade dependencies (#59)

--- src/Playground.js
+++ src/Playground.js
@@ -18,24 +18,28 @@ function Playground() {
 
     return (
         <Box
-            bg="canvas.default"
-            width="100%"
-            minHeight="100vh"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            p={5}
+            sx={{
+                bg: 'canvas.default',
+                width: '100%',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                p: 5,
+            }}
         >
             <MarkGithubIcon size={24} />
             <Box
-                maxWidth={600}
-                width="100%"
-                height={300}
-                bg="neutral.emphasisPlus"
-                borderRadius={2}
-                p={4}
-                my={6}
+                sx={{
+                    maxWidth: 600,
+                    width: '100%',
+                    height: 300,
+                    bg: 'neutral.emphasisPlus',
+                    borderRadius: 2,
+                    p: 4,
+                    my: 6,
+                }}
             >
                 <CodeLine icon={CheckIcon} iconColor="success.fg">
                     Mona's playground successfully initialised...
@@ -60,11 +64,18 @@ function Playground() {
 
 function CodeLine({ icon, iconColor, children }) {
     return (
-        <Box display="flex" color="fg.onEmphasis" mb={2}>
-            <Box display="flex" mt="2px" width={20} minWidth={20}>
-                <StyledOcticon icon={icon} size={16} color={iconColor} />
+        <Box sx={{ display: 'flex', color: 'fg.onEmphasis', mb: 2 }}>
+            <Box sx={{ display: 'flex', mt: '2px', width: 20, minWidth: 20 }}>
+                <StyledOcticon
+                    icon={icon}
+                    size={16}
+                    sx={{ color: iconColor }}
+                />
             </Box>
-            <Text as="p" flex={1} fontSize={1} fontFamily="mono" ml={2}>
+            <Text
+                as="p"
+                sx={{ flex: 1, fontSize: 1, fontFamily: 'mono', ml: 2 }}
+            >
                 {children}
             </Text>
         </Box>
@@ -73,15 +84,14 @@ function CodeLine({ icon, iconColor, children }) {
 
 function Footer() {
     return (
-        <Box textAlign="center">
-            <Box mr={2} display="inline-block">
+        <Box sx={{ textAlign: 'center' }}>
+            <Box sx={{ mr: 2, display: 'inline-block' }}>
                 <StyledOcticon
                     icon={MortarBoardIcon}
                     size={16}
-                    color="attention.fg"
-                    sx={{ mr: 1 }}
+                    sx={{ mr: 1, color: 'attention.fg' }}
                 />
-                <Text color="attention.fg">Tip</Text>
+                <Text sx={{ color: 'attention.fg' }}>Tip</Text>
             </Box>
             <Text>
                 Before you get started check out our{' '}
