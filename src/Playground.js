26a2852580996c89d6e06401083be726ff40a3c8
Maxime De Greve <maximedegreve@me.com>
2024-05-01 15:52:54 +0100

    Kickoff

--- src/Playground.js
+++ src/Playground.js
@@ -6,96 +6,78 @@ import {
     MortarBoardIcon,
 } from '@primer/octicons-react'
 
+import styles from './Playground.module.css'
+
 import MonaLoadingImage from './images/mona-loading.gif'
 
 function Playground() {
     /*
     WELCOME TO MONA's 😽🐙 PLAYGROUND
     Delete everything in here or play with the existing Mona playground code to get familiar with Primer React.
-    Documentation: https://primer.style/react
-    Documentation colors: https://primer.style/primitives/colors
+    You can style the elements through the `Playground.module.css` file.
+    Documentation: https://primer.style/guides/react
   */
 
     return (
-        <Box
-            sx={{
-                bg: 'canvas.default',
-                width: '100%',
-                minHeight: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                p: 5,
-            }}
-        >
+        <div className={styles.page}>
             <MarkGithubIcon size={24} />
-            <Box
-                sx={{
-                    maxWidth: 600,
-                    width: '100%',
-                    height: 300,
-                    bg: 'neutral.emphasisPlus',
-                    borderRadius: 2,
-                    p: 4,
-                    my: 6,
-                }}
-            >
-                <CodeLine icon={CheckIcon} iconColor="success.fg">
+            <ul className={styles.terminal}>
+                <CodeLine
+                    icon={CheckIcon}
+                    iconColor="var(--display-green-fgColor)"
+                >
                     Mona's playground successfully initialised...
                 </CodeLine>
-                <CodeLine icon={CommentIcon} iconColor="accent.fg">
+                <CodeLine
+                    icon={CommentIcon}
+                    iconColor="var(--display-blue-fgColor)"
+                >
                     Visit <Text color="text.warning">src/Playground.js</Text>{' '}
                     and start building your own layouts using Primer.
                 </CodeLine>
-                <Box display="inline-block" ml={3} mt={2}>
+                <li className={styles.loader}>
                     <img
                         src={MonaLoadingImage}
-                        alt="mona"
+                        alt="Loading animation"
                         width={48}
                         height={48}
                     />
-                </Box>
-            </Box>
+                </li>
+            </ul>
             <Footer />
-        </Box>
+        </div>
     )
 }
 
 function CodeLine({ icon, iconColor, children }) {
     return (
-        <Box sx={{ display: 'flex', color: 'fg.onEmphasis', mb: 2 }}>
-            <Box sx={{ display: 'flex', mt: '2px', width: 20, minWidth: 20 }}>
-                <Octicon icon={icon} size={16} sx={{ color: iconColor }} />
-            </Box>
-            <Text
-                as="p"
-                sx={{ flex: 1, fontSize: 1, fontFamily: 'mono', ml: 2 }}
-            >
-                {children}
-            </Text>
-        </Box>
+        <li className={styles.codeline}>
+            <div className={styles.codelineIcon} style={{ color: iconColor }}>
+                <Octicon icon={icon} size={16} />
+            </div>
+            <div className={styles.codelineText}>{children}</div>
+        </li>
     )
 }
 
 function Footer() {
     return (
-        <Box sx={{ textAlign: 'center' }}>
-            <Box sx={{ mr: 2, display: 'inline-block' }}>
+        <footer className={styles.footer}>
+            <div className={styles.tip}>
                 <Octicon
                     icon={MortarBoardIcon}
                     size={16}
-                    sx={{ mr: 1, color: 'attention.fg' }}
+                    className={styles.tipIcon}
                 />
-                <Text sx={{ color: 'attention.fg' }}>Tip</Text>
-            </Box>
+                <span>Tip</span>
+            </div>
             <Text>
                 Before you get started check out our{' '}
-                <Link href="https://primer.style/react" target="_blank">
+                <Link href="https://primer.style/guides/react" target="_blank">
                     Primer React Documentation
                 </Link>
             </Text>
-        </Box>
+        </footer>
     )
 }
 
