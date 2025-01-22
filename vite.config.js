7751a7a7df45a9cc1ee1e8917103170f32e30741
Daniel Fosco <dfosco@github.com>
2025-01-22 09:43:56 +0100

    Update localhost port

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import generouted from '@generouted/react-router/plugin'
 
 export default defineConfig({ 
-  plugins: [react(), generouted()]
+  plugins: [react(), generouted()],
+  server: { port: 1234 },
 })
