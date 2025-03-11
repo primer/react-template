06f41e7b81fd8a042592086741e9ad5205732707
Daniel Fosco <dfosco@github.com>
2025-03-11 18:32:18 +0100

    Re-add Playground as index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,39 +1,12 @@
-import { Box, Text, Link, Octicon } from '@primer/react'
-import { CheckIcon } from '@primer/octicons-react'
-import Application from '../templates/Application/Application.jsx'
-
-import {
-  IssueOpenedIcon,
-  ThreeBarsIcon,
-  CodeIcon,
-  GitPullRequestIcon,
-  CommentDiscussionIcon,
-  ShieldIcon,
-  PlayIcon,
-  ProjectIcon,
-  GraphIcon,
-  MarkGithubIcon,
-  SmileyIcon,
-  PersonIcon,
-  MentionIcon
-} from '@primer/octicons-react'
-
-const topnav = [
-  { icon: CodeIcon, label: 'Code', url: '/', current: true },
-  { icon: IssueOpenedIcon, label: 'Issues', counter: 10, url: '/issues' },
-  { icon: GitPullRequestIcon, label: 'Pull Requests', counter: 3, url: '/overview' },
-  { icon: CommentDiscussionIcon, label: 'Discussions' },
-  { icon: PlayIcon, label: 'Actions' },
-  { icon: ProjectIcon, label: 'Projects', counter: 7 },
-  { icon: ShieldIcon, label: 'Security', counter: 12 },
-  { icon: GraphIcon, label: 'Insights' }
-];
+import Playground from '../components/Playground.jsx'
+import ColorModeSwitcher from '../components/ColorModeSwitcher.jsx'
 
 function Code() {
     return (
-      <Application title="Primer" subtitle="React" topnav={topnav}>
-          This is the code page
-      </Application>
+      <>
+        <Playground />
+        <ColorModeSwitcher />
+      </>
     )
 }
 
--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,8 @@ import { components, hooks, utils } from '@generouted/react-router/client'
 
 export type Path =
   | `/`
-  | `/issues`
-  | `/overview`
+  | `/Issues`
+  | `/Overview`
 
 export type Params = {
   
