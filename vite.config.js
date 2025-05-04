4eed68a7f64bb39f45ff41490d68fa98f71642c1
Maxime De Greve <maximedegreve@me.com>
2025-05-04 18:27:10 +0100

    Something is wrong...

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,23 @@ import generouted from '@generouted/react-router/plugin'
 import postcssCustomMedia from 'postcss-custom-media'
 import postcssNesting from 'postcss-nesting'
 import postcssGlobalData from '@csstools/postcss-global-data'
+import { glob } from 'glob'
+import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [
         react(),
         generouted(),
-        postcssNesting(),
-        postcssCustomMedia(),
         postcssGlobalData({
-            files: ['./node_modules/@primer/primitives/dist/css/**/*.css'],
+            files: glob.sync(
+                resolve(
+                    __dirname,
+                    './node_modules/@primer/primitives/dist/css/**/*.css'
+                )
+            ),
         }),
+        postcssCustomMedia(),
+        postcssNesting({ edition: '2024-02' }),
     ],
     server: { port: 1234 },
 })
