bba06b6f483a54e2fa11360146994b86b9542108
Maxime De Greve <maximedegreve@me.com>
2025-05-04 17:27:26 +0100

    Start adding nesting for queries

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import generouted from '@generouted/react-router/plugin'
+import postcssCustomMedia from 'postcss-custom-media'
+import postcssNesting from 'postcss-nesting'
+import postcssGlobalData from '@csstools/postcss-global-data'
 
-export default defineConfig({ 
-  plugins: [react(), generouted()],
-  server: { port: 1234 },
+export default defineConfig({
+    plugins: [
+        react(),
+        generouted(),
+        postcssNesting(),
+        postcssCustomMedia(),
+        postcssGlobalData({
+            files: ['./node_modules/@primer/primitives/dist/css/**/*.css'],
+        }),
+    ],
+    server: { port: 1234 },
 })
